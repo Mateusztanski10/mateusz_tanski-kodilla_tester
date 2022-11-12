e41ff3322ce20c56a32c056ea3facfeae0608ece
Mateusz Tanski <mateusztanski99@op.pl>
2022-11-12 20:09:37 +0100

    Instrukcje warunkowe

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,21 +1,18 @@
 public class FirstClass {
     public static void main(String[] args) {
         Notebook notebook = new Notebook(600, 2000, 2020);
+        Notebook heavyNotebook = new Notebook(2000,500, 1995);
+        Notebook oldNotebook = new Notebook(1600, 1500, 2010);
+
+        displayNotebookParameters(notebook);
+        displayNotebookParameters(heavyNotebook);
+        displayNotebookParameters(oldNotebook);
+    }
+
+    private static void displayNotebookParameters(Notebook notebook) {
         System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
         notebook.checkPrice();
         notebook.checkWeight();
         notebook.checkYear();
-
-        Notebook heavyNotebook = new Notebook(2000,500, 1995);
-        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price + " " + heavyNotebook.year);
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.checkYear();
-
-        Notebook oldNotebook = new Notebook(1600, 1500, 2010);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + oldNotebook.year);
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.checkYear();
     }
 }
