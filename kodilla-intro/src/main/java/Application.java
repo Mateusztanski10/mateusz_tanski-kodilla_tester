c3efefb03139516e003075b88195e9e7191f67f6
Mateusz Tanski <mateusztanski99@op.pl>
2022-11-21 18:28:27 +0100

    Prace domowe

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,6 +1,6 @@
 public class Application {
     public static void main(String[] args) {
-        Work adam = new Work("Adam", 40.5, 178);
+        UserCreator adam = new UserCreator("Adam", 40.5, 178);
         adam.checkAge();
     }
 }
