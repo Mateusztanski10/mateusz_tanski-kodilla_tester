1f2ce9b06917e85537606c4bb2aa4e33242d8d31
Mateusz Tanski <mateusztanski99@op.pl>
2023-01-04 16:17:07 +0100

    Kodilla-advanced-tests

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework2/GamblingMachineTestSuite.java
@@ -0,0 +1,20 @@
+package com.kodilla.parametrized_tests.homework2;
+
+import com.kodilla.parametrized_tests.ParameterizedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamblingMachineTestSuite {
+
+    private GamblingMachine gamblingMachine = new GamblingMachine();
+
+    @Test
+    @ParameterizedTest
+    @CsvFileSource(resources = "/allNumbersForGamblingMachine.csv")
+    public void shouldEqualsMyNumbersWithComputerNumbers(int input) {
+        assertEquals(input, gamblingMachine.howManyWins());
+    }
+
+}
