916b45370763e53135de6b5669f53800f5be72da
Mateusz Tanski <mateusztanski99@op.pl>
2023-01-08 21:47:01 +0100

    Kodilla-collections-advanced

--- kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuite.java
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuite.java
@@ -1,31 +1,34 @@
 package com.kodilla.collections.adv.exercises.homework;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class FlightFinderTestSuite {
 
     static FlightFinder flightFinder;
 
     @BeforeAll
-
     public static void setUp() {
         flightFinder = new FlightFinder();
     }
 
     @Test
-
     public void testFindFlightsFrom() {
         assertEquals(2, flightFinder.findFlightsFrom("London").size());
     }
 
     @Test
-
     public void testFindFlightsTo() {
         assertEquals(3, flightFinder.findFlightsTo("Paris").size());
+    }
+    @Test
+    public void testFindFallsFlight() {
+        assertFalse(2 == flightFinder.findFlightsTo("Paris").size());
     }
 }
