c4a5a06701726ad5f7ad9f2f2fb77495be7cb178
Mateusz Tanski <mateusztanski99@op.pl>
2023-02-01 22:06:23 +0100

    Kodilla-advanced-tests

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -1,19 +1,11 @@
 package com.kodilla.parametrized_tests.homework;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonTestSuite {
-
-    static private Person person;
-
-    @BeforeAll
-    static public void setUp() {
-        person = new Person(1.90, 80.8);
-    }
 
     @ParameterizedTest
     @MethodSource(value = "com.kodilla.parametrized_tests.homework.DataForPersonTestClass#dataForTesting")
