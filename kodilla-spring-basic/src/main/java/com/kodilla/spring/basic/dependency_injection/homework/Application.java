cdebf65f763a4a6ecf1f9c41ad59704cbc2ea336
Mateusz Tanski <mateusztanski99@op.pl>
2023-02-01 21:38:41 +0100

    Kodilla-spring-basic

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -0,0 +1,12 @@
+
+package com.kodilla.spring.basic.dependency_injection.homework;
+
+public class Application {
+
+    public static void main(String[] args) {
+        DeliveryService deliveryService = new SendPackageFromPoland();
+        NotificationService notificationService = new AustraliaNotificationService();
+        ShippingCenter shippingCenter = new ShippingCenter(deliveryService, notificationService);
+        shippingCenter.sendPackage("Sydney", 70);
+    }
+}
