d282343b3aac3dda7f9582e56eafea5ea777d93a
Mateusz Tanski <mateusztanski99@op.pl>
2023-02-06 20:51:17 +0100

    Kodilla-rest-api

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
@@ -14,30 +14,28 @@ class BookControllerTest {
 
     @Test
     void shouldFetchBooks() {
-        //given
         BookService bookServiceMock = Mockito.mock(BookService.class);
         BookController bookController = new BookController(bookServiceMock);
         List<BookDto> booksList = new ArrayList<>();
         booksList.add(new BookDto("Title 1", "Author 1"));
         booksList.add(new BookDto("Title 2", "Author 2"));
         Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
-        //when
+
         List<BookDto> result = bookController.getBooks();
-        //then
+
         assertThat(result).hasSize(2);
     }
 
     @Test
     void shouldAddBooks() {
-        //given
         BookService bookServiceMock = Mockito.mock(BookService.class);
         BookController bookController = new BookController(bookServiceMock);
         bookController.addBook(new BookDto("The Lord Of The Rings", "J.R.R. Tolkien"));
         bookController.addBook(new BookDto("Bastion", "Stephen King"));
         bookController.addBook(new BookDto("It", "Stephen King"));
-        //when
+
         List<BookDto> result = bookController.getBooks();
-        //then
+
         assertThat(result).hasSize(3);
     }
 }
